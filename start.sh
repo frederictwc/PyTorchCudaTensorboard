c0c0fae4a5f317e46a3546dcb9d17db11a1f5881
frederictwc <frederictwc@live.com>
2020-09-08 22:43:28 +0800

    update directory

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 docker stop pytorch_cuda
 docker rm pytorch_cuda
 docker image rm pytorch_cuda
-docker build -t pytorch_cuda .
+cd ../
+docker build -t pytorch_cuda -f pytorch_cuda_tensorboard/Dockerfile .
 docker run --gpus all -d -p 6006:6006 \
 -v $(pwd):/workspace/src/ \
 --name pytorch_cuda \
